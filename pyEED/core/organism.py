46ba5a01b0e30cb21752bd196f62dfee7b6d5939
gfeuerriegel <105804626+gfeuerriegel@users.noreply.github.com>
2022-11-10 11:33:12 +0100

    Add files via upload

---
+++ pyEED/core/organism.py
@@ -0,0 +1,32 @@
+import sdRDM
+
+from typing import Optional, Union
+from pydantic import PrivateAttr
+from pydantic import Field
+from sdRDM.base.listplus import ListPlus
+from sdRDM.base.utils import forge_signature, IDGenerator
+
+
+@forge_signature
+class Organism(sdRDM.DataModel):
+    id: str = Field(
+        description="Unique identifier of the given object.",
+        default_factory=IDGenerator("organismINDEX"),
+        xml="@id",
+    )
+
+    name: str = Field(
+        ..., description="Name of the organism"
+    )
+    
+    ncbi_taxonomy_id: Optional[int] = Field(
+        ..., description="NCBI Taxonomy ID to identify the organism"
+    )
+
+    __repo__: Optional[str] = PrivateAttr(
+        default="git://github.com/PyEED/pyeed-data-model.git"
+    )
+
+    __commit__: Optional[str] = PrivateAttr(
+        default="047f17317fa860206980a47dc3790cbc3204f343"
+    )
